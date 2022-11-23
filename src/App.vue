2857f72f47c74c124ff881fed54ea3272285ddc2
Gennaro Manzo <gennaromanzo1993@gmail.com>
2022-11-23 17:58:09 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Searchbar text="Search an image"></Searchbar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Searchbar from './components/Search-bar.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {};
+  },
+  methods: {},
+  components: { Searchbar },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Search-bar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="input-container">
+    <input
+      type="text"
+      required
+      name="input"
+      id="input-query"
+      placeholder="text"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    text: String,
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+* {
+  display: flex;
+  justify-content: center;
+  margin: 2rem;
+  padding: 0.6rem;
+}
+#input-query {
+  width: 50vw;
+  text-align: center;
+  margin: auto;
+}
+#input-query:focus {
+  outline: none;
+}
+#input-query:focus::placeholder {
+  color: transparent;
+}
+#input-cointainer {
+  display: flex;
+  justify-content: center;
+  border: 1px solid #000;
+  margin: auto;
+}
+</style>
